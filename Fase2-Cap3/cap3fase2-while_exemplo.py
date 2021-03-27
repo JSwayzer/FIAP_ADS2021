6e6252f46eb17fb7b5505feae0517446f0a2383c
JSwayzer <jswayzer@live.com>
2021-03-27 16:17:54 -0300

    Add files via upload

---
+++ Fase2-Cap3/cap3fase2-while_exemplo.py
@@ -0,0 +1,21 @@
+#Variáveis:
+#    resposta: alfanumérico
+#Início
+#    resposta = “0”
+#    Enquanto (resposta != “42”) faça
+#        Escreva “Qual é a resposta para a vida, o universo e tudo mais?”
+#        Leia resposta
+#    Fim_enquanto
+#    Escreva “Parabéns, você acertou!”
+#Fim
+
+#criação da variável com um valor inicial
+resposta = "0"
+#enquanto a resposta não for 42, a repetição ocorre
+while (resposta != "42"):
+    #para cada uma das repetições, o usuário vai submeter uma resposta
+    resposta = input("Qual a resposta para a vida, o universo e tudo mais?")
+#quanto o laço terminar, a mensagem é exibida
+print("Parabéns, você acertou!")
+
+
---
+++ Fase2-Cap3/cap3fase2-while_variavel_contadora.py
@@ -0,0 +1,18 @@
+#Variáveis:
+#    i: inteiro
+#Início
+#    i = 0
+#    Enquanto (i<10) faça
+#        Escreva “Mais uma mensagem, com i valendo: ”, i
+#        i = i + 1
+#    Fim_enquanto
+#Fim
+
+
+#criação da variável com um valor inicial
+i = 0
+#enquanto a variável contadora não chegar ao limite
+while (i<10):
+    #para cada uma das repetições uma mensagem é exibida
+    print("Mais uma mensagem, com i valendo: {}".format(i))
+    i = i + 1
