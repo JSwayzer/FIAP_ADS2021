d6ed932db5cf726ea490670758e9046ae5f477b5
JSwayzer <jswayzer@live.com>
2021-03-27 16:17:33 -0300

    Create cap3fase2-for_exemplo.py

---
+++ Fase2-Cap3/cap3fase2-for_exemplo.py
@@ -0,0 +1,18 @@
+#para <contadora> de <valor inicial> até <valor final> passo <incremento> faça
+#    //instruções que serão repetidas
+#fim_para
+
+#a variável contadora deverá assumir valores no intervalo entre 0 e 9
+for x in range(10):
+    #para cada um desses valores, vamos printar o valor da variável
+    print(x)
+
+#a variável contadora deverá assumir valores no intervalo entre 1 e 14
+for x in range(1,15):
+    #para cada um desses valores, vamos printar o valor da variável
+    print(x)
+
+#a variável contadora deverá assumir valores no intervalo entre 1 e 9 com incremento 2
+for x in range(1,10,2):
+    #para cada um desses valores, vamos printar o valor da variável
+    print(x)
