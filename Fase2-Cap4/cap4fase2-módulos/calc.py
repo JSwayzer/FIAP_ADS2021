9c786e3b5a76d0eee814cb001d5c2b1634f8dfe5
JSwayzer <jswayzer@live.com>
2021-03-28 21:42:27 -0300

    Create calc.py

---
+++ Fase2-Cap4/cap4fase2-módulos/calc.py
@@ -0,0 +1,41 @@
+# função de soma
+def somar(a, b):
+    return float(a) + float(b)
+
+
+# função de subtração
+def subtrair(a, b):
+    return float(a) - float(b)
+
+
+# função de divisão
+def dividir(a, b):
+    if b != 0:
+        return float(a) / float(b)
+    return 0
+
+
+# função de multiplicar
+def multiplicar(a, b):
+    return float(a) * float(b)
+
+# função de soma
+# def somar(a, b):
+#    return a+b
+
+
+# função de subtração
+# def subtrair(a, b):
+#    return a-b
+
+
+# função de divisão
+# def dividir(a, b):
+#    if(b!=0):
+#        return a/b
+#    return 0
+
+
+# função de multiplicar
+# def multiplicar(a, b):
+#    return a*b
